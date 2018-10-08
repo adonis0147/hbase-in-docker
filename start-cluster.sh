e1eb04b9aa5f9ebf6bc9e446e4980e3b3510b8ba
Adonis Ling <adonis0147@gmail.com>
2018-10-08 14:58:56 +0800

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -10,26 +10,37 @@ if ! docker network ls | grep hadoop > /dev/null; then
 fi
 
 docker run -itd \
-  --network hadoop \
-  --hostname hbase-cluster-n1.com \
-  --name hbase-cluster-n1.com \
-  --mount source=home1,target=/home/hadoop \
-  --mount source=disk1,target=/data \
+  --network=hadoop \
+  --hostname=hbase-cluster-n1.com \
+  --name=hbase-cluster-n1 \
+  --mount=source=home1,target=/home/hadoop \
+  --mount=source=disk1,target=/data \
+  --ip=172.18.0.2 \
+  --add-host=hbase-cluster-n1.com:172.18.0.2 \
+  --add-host=hbase-cluster-n2.com:172.18.0.3 \
+  --add-host=hbase-cluster-n3.com:172.18.0.4 \
   hbase-in-docker
 
 docker run -itd \
-  --network hadoop \
-  --hostname hbase-cluster-n2.com \
-  --name hbase-cluster-n2.com \
-  --mount source=home2,target=/home/hadoop \
-  --mount source=disk2,target=/data \
+  --network=hadoop \
+  --hostname=hbase-cluster-n2.com \
+  --name=hbase-cluster-n2 \
+  --mount=source=home2,target=/home/hadoop \
+  --mount=source=disk2,target=/data \
+  --ip=172.18.0.3 \
+  --add-host=hbase-cluster-n1.com:172.18.0.2 \
+  --add-host=hbase-cluster-n2.com:172.18.0.3 \
+  --add-host=hbase-cluster-n3.com:172.18.0.4 \
   hbase-in-docker
 
 docker run -itd \
-  --network hadoop \
-  --hostname hbase-cluster-n3.com \
-  --name hbase-cluster-n3.com \
-  --mount source=home3,target=/home/hadoop \
-  --mount source=disk3,target=/data \
+  --network=hadoop \
+  --hostname=hbase-cluster-n3.com \
+  --name=hbase-cluster-n3 \
+  --mount=source=home3,target=/home/hadoop \
+  --mount=source=disk3,target=/data \
+  --ip=172.18.0.4 \
+  --add-host=hbase-cluster-n1.com:172.18.0.2 \
+  --add-host=hbase-cluster-n2.com:172.18.0.3 \
+  --add-host=hbase-cluster-n3.com:172.18.0.4 \
   hbase-in-docker
-
