f76c6ee2f6462a5ec1c4cb2494283ae8e2f54b51
Adonis Ling <adonis0147@gmail.com>
2020-01-26 09:40:49 +0000

    Update install-zookeeper.sh

--- install/scripts/install-zookeeper.sh
+++ install/scripts/install-zookeeper.sh
@@ -15,6 +15,11 @@ chown -R hadoop:hadoop "${HOME}/zookeeper-current"
 sudo -u hadoop mkdir -p /data/conf
 sudo -u hadoop mv "${zookeeper_conf_path}" /data/conf/zookeeper-conf
 sudo -u hadoop ln -snf /data/conf/zookeeper-conf "${zookeeper_conf_path}"
+sudo -u hadoop mv "${HOME}/zookeeper-current/bin/zkEnv.sh" /data/conf/zookeeper-conf
 
 command='/${ZOO_LOG_DIR}/i ZOO_LOG_DIR='"${HOME}"'/cluster-data/zk/logs'
-sudo -u hadoop sed -i "${command}" "${HOME}/zookeeper-current/bin/zkEnv.sh"
+sudo -u hadoop sed -i "${command}" /data/conf/zookeeper-conf/zkEnv.sh
+sudo -u hadoop ln -snf ../conf/zkEnv.sh "${HOME}/zookeeper-current/bin/"
+
+zookeeper_bin_path="/home/hadoop/zookeeper-current/bin"
+sudo -u hadoop echo 'export PATH="'"${zookeeper_bin_path}"':${PATH}"' >> /home/hadoop/.bash_profile
